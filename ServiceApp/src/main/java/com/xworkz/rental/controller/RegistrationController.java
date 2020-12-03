65a535128661b9d922cfb4c4dddffa7c555d8fe2
X-workzDev <73648583+x-workzdev@users.noreply.github.com>
2020-12-03 21:44:43 +0530

    Update RegistrationController.java

--- ServiceApp/src/main/java/com/xworkz/rental/controller/RegistrationController.java
+++ ServiceApp/src/main/java/com/xworkz/rental/controller/RegistrationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import com.xworkz.rental.dto.RegistrationDTO;
 import com.xworkz.rental.entity.RegistrationEntity;
 import com.xworkz.rental.service.RegistrationService;
